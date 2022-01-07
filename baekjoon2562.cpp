11c93a8ddbcf51bdd5da07432b35e72165989b47
elizabethgim <yujingim@kookmin.ac.kr>
2022-01-07 18:35:20 +0900

    Create baekjoon2562.cpp

---
+++ baekjoon2562.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
+    int i, num, min, max;
+    min = 0;
+    max = 0;
+
+    for(i=0;i<9;i++)
+    {
+        std::cin>>num;
+        
+    }
+}
