4cbace1228b39782d09d57f454d476f9d4921f69
elizabethgim <yujingim@kookmin.ac.kr>
2022-01-09 21:56:40 +0900

    Update baekjoon2562.cpp

--- baekjoon2562.cpp
+++ baekjoon2562.cpp
@@ -5,13 +5,19 @@ int main()
     std::cin.tie(NULL);
     std::ios::sync_with_stdio(false);
 
-    int i, num, min, max;
-    min = 0;
+    int i, num, cnt, max;
+    cnt = 0;
     max = 0;
 
     for(i=0;i<9;i++)
     {
         std::cin>>num;
-        
+        if(num > max){
+            max = num;
+            cnt = i+1;
+        }
     }
+
+    std::cout<<max<<"\n";
+    std::cout<<cnt<<"\n";
 }
