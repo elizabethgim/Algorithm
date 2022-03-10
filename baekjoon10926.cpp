23f183d78b2e266df05a86e27df7f09e08483267
Yujin <yujingim@kookmin.ac.kr>
2022-03-11 00:00:33 +0900

    미완

---
+++ baekjoon10926.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string id;
+    cin >> id;
+    cout << id <<"\n";
+    string res = id+"??!";
+    cout << res << "\n";
+
+}
