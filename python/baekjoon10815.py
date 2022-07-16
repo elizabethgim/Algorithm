06d42d08b9a76928e694390382c274cae4e5ce80
elizabethgim <yujingim@kookmin.ac.kr>
2022-07-17 00:06:18 +0900

    시도

---
+++ python/baekjoon10815.py
@@ -0,0 +1,9 @@
+import sys
+sanganCardNum = int(sys.stdin.readline())
+sanganCards = map(int, input().split(" "))
+cardNum = int(sys.stdin.readline())
+cards = map(int, input().split(" "))
+print(sanganCardNum)
+print(sanganCards)
+print(cardNum)
+print(cards)
