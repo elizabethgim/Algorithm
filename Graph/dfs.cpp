dcdf6ee22c8f0ede8c235e9d36bca94f82f5797b
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-27 20:49:39 +0900

    dfs

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,1 +1,50 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+bool visited[9];
+vector<int> graph[9];
+
+void dfs(int x){
+    visited[x] = true;
+    cout << x << " ";
+    for(int i=0;i< graph[x].size(); i++){
+        int y = graph[x][i];
+        if(!visited[y]){
+            dfs(y);
+        }
+    }
+}
+
+int main(void)
+{
+    graph[1].push_back(2);
+    graph[1].push_back(3);
+    graph[1].push_back(8);
+
+    graph[2].push_back(1);
+    graph[2].push_back(7);
+
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+
+    graph[6].push_back(7);
+
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+
+    dfs(1);
+}
+
+// https://better-tomorrow.tistory.com/entry/DFS-BFS-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
