a159fd7e132731ea7338b2cfa9df80925d51de1e
elizabethgim <yujingim@naver.com>
2023-03-28 13:46:03 +0900

    Delete: 'Bronze' directory

--- baekjoon/Bronze/bj1546.py
+++
@@ -1,8 +0,0 @@
-import sys
-N = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-newArr = []
-for a in arr:
-    newArr.append(a/max(arr)*100)
-# print(newArr)
-print(sum(newArr)/N)
--- baekjoon/Bronze/bj3003.py
+++
@@ -1,6 +0,0 @@
-chess = [1, 1, 2, 2, 2, 8]
-count = list(map(int, input().split()))
-result = []
-for i in range(len(chess)):
-    result.append(chess[i] - count[i])
-print(*result, sep=" ")
