1d7c9370438a4fe04488dd644439267f5f937cae
elizabethgim <yujingim@naver.com>
2024-08-26 22:22:05 +0900

    [백준, 20114] 미아 노트 (36ms, 31120KB)

--- baekjoon/20114_미아_노트.py
+++ baekjoon/20114_미아_노트.py
@@ -1,12 +1,9 @@
-from collections import deque
-
 N, H, W = map(int, input().split())
 
 words = []
 for i in range(H):
     words.append(list(j for j in input().strip()))
 
-words2 = [['' for _ in range(N*W)] for _ in range(H)]
 visited = [[False for _ in range(N*W)] for _ in range(H)]
 
 answer = ""
@@ -15,27 +12,16 @@ for i in range(H):
     for j in range(N*W):
         if not visited[i][j]:
             visited[i][j] = True
-            words2[i][j] = words[i][j]
-            answer += words[i][j]
+            tmp = words[i][j]
 
             for ii in range(H):
                 for jj in range(W):
                     n = i+ii
                     m = j+jj
-                    words2[n][m] = words[i][j]
                     visited[n][m] = True
+                    if tmp == '?' and tmp != words[n][m]:
+                        tmp = words[n][m]
+
+            answer += tmp
 
 print(answer)
-
-
-def bfs(start):
-    queue = deque()
-    queue.append(start)
-
-    while queue:
-        x, y = queue.popleft()
-        visited[x][y] = True
-        
-        for i in range(H):
-            for j in range(W):
-                visited[i][j] = True
